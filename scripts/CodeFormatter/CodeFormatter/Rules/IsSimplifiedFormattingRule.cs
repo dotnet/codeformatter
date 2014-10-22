940dd747aea0e3e26e536de0bdc4110ce049850d
Immo Landwerth <immol@microsoft.com>
2014-10-22 14:04:23 -0700

    Make simplifier work

--- scripts/CodeFormatter/CodeFormatter/Rules/IsSimplifiedFormattingRule.cs
+++ scripts/CodeFormatter/CodeFormatter/Rules/IsSimplifiedFormattingRule.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CodeFormatter.Engine;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Simplification;
 
 namespace CodeFormatter.Rules
@@ -12,9 +13,32 @@ namespace CodeFormatter.Rules
     [ExportFormattingRule(Int32.MaxValue / 2)]
     internal sealed class IsSimplifiedFormattingRule : IFormattingRule
     {
-        public Task<Document> ProcessAsync(CancellationToken cancellationToken, Document document)
+        public async Task<Document> ProcessAsync(CancellationToken cancellationToken, Document document)
         {
-            return Simplifier.ReduceAsync(document, cancellationToken: cancellationToken);
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+
+            // The simplifier will only simplify nodes that are annotated with a
+            // specific annotation. Right now, there is no API on the simplifier
+            // to say "ignore all annotations and reduce everything". The
+            // workaround is to simply annotate each node. Yep, that's not going
+            // to make the GC happy but it will totally get the job done.
+            var annotater = new Annotater();
+            var annoatedRoot = annotater.Visit(root);
+            var annotatedDocument = document.WithSyntaxRoot(annoatedRoot);
+
+            return await Simplifier.ReduceAsync(annotatedDocument, cancellationToken: cancellationToken);
+        }
+
+        private sealed class Annotater : CSharpSyntaxRewriter
+        {
+            public override SyntaxNode Visit(SyntaxNode node)
+            {
+                if (node == null)
+                    return null;
+
+                var rewrittenNode = base.Visit(node);
+                return rewrittenNode.WithAdditionalAnnotations(Simplifier.Annotation);
+            }
         }
     }
 }
