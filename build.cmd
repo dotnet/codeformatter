5cb0853913a78c9f0f6009b2baf8929abc7ae5c7
Immo Landwerth <immol@microsoft.com>
2014-10-30 15:23:05 -0700

    Add basic build.cmd

---
+++ build.cmd
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+SETLOCAL
+
+SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+SET SOLUTION_PATH=%~dp0src\BuildToolsInternal.sln
+
+IF EXIST %CACHED_NUGET% goto restore
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+
+:restore
+IF EXIST "%~dp0src\packages" goto build
+%CACHED_NUGET% restore %SOLUTION_PATH%
+
+:build
+
+"%ProgramFiles(x86)%\MSBuild\12.0\bin\MSBuild.exe" %SOLUTION_PATH% /p:OutDir="%~dp0bin " /nologo /m /v:m /flp:verbosity=normal %*
