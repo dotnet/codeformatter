b22acb20cd497a1b758722f50e7521ba09c00b2b
Immo Landwerth <immol@microsoft.com>
2014-11-05 14:50:30 -0800

    Simplify header after syncing with LCA

--- src/Microsoft.DotNet.CodeFormatting/Rules/HasCopyrightHeaderFormattingRule.cs
+++ src/Microsoft.DotNet.CodeFormatting/Rules/HasCopyrightHeaderFormattingRule.cs
@@ -18,10 +18,7 @@ namespace Microsoft.DotNet.CodeFormatting.Rules
         static readonly string[] CopyrightHeader =
         {
             "// Copyright (c) Microsoft. All rights reserved.",
-            "// This code is licensed under the MIT License.",
-            "// THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR",
-            "// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,",
-            "// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT."
+            "// Licensed under the MIT license. See LICENSE file in the project root for full license information."
         };
 
         public async Task<Document> ProcessAsync(Document document, CancellationToken cancellationToken)
