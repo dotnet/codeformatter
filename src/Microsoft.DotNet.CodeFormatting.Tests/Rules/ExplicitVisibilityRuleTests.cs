a89aa10b0a6408e0977a7530e0abe3f4b6040cc3
Jared Parsons <jaredpar@microsoft.com>
2015-01-07 09:24:59 -0800

    Initial pass at explicit visibility rule
    This rule adds explicit visibility enforcement.  Right now it supports
    types and method declarations.  Need to expand out into other members
    (properties, events, etc ...).

---
+++ src/Microsoft.DotNet.CodeFormatting.Tests/Rules/ExplicitVisibilityRuleTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.DotNet.CodeFormatting.Tests
+{
+    public sealed class ExplicitVisibilityRuleTests : CodeFormattingTestBase
+    {
+        internal override IFormattingRule GetFormattingRule()
+        {
+            return new Rules.ExplicitVisibilityRule();
+        }
+
+        [Fact]
+        public void TestTypeVisibility()
+        {
+            var text = @"
+class C1 { } 
+sealed partial class C2 { } 
+struct S1 { }
+interface I1 { } 
+enum E1 { }
+delegate void D1() { } 
+";
+
+            var expected = @"
+internal class C1 { } 
+internal sealed partial class C2 { } 
+internal struct S1 { }
+internal interface I1 { } 
+internal enum E1 { }
+internal delegate void D1() { } 
+";
+
+            Verify(text, expected, runFormatter: false);
+        }
+
+        [Fact]
+        public void TestNestedTypeVisibility()
+        {
+            var text = @"
+class C
+{
+    class C { } 
+    struct S { } 
+    interface I { } 
+    enum E { } 
+    delegate void D() { } 
+}
+";
+
+            var expected = @"
+internal class C
+{
+    private class C { } 
+    private struct S { } 
+    private interface I { } 
+    private enum E { } 
+    private delegate void D() { } 
+}
+";
+
+            Verify(text, expected, runFormatter: false);
+        }
+
+        [Fact]
+        public void TestMethodVisibility()
+        {
+            var text = @"
+internal class C
+{
+    void M1();
+    internal void M2();
+}
+
+internal struct S
+{
+    void M1();
+    internal void M2();
+}
+
+internal interface I
+{
+    void M1();
+    void M2();
+}
+";
+
+            var expected = @"
+internal class C
+{
+    private void M1();
+    internal void M2();
+}
+
+internal struct S
+{
+    private void M1();
+    internal void M2();
+}
+
+internal interface I
+{
+    void M1();
+    void M2();
+}
+";
+
+                Verify(text, expected, runFormatter: false);
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.CodeFormatting/Rules/ExplicitVisibilityRule.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.CodeFormatting.Rules
+{
+    internal sealed class ExplicitVisibilityRule : IFormattingRule
+    {
+        private sealed class VisibilityRewriter : CSharpSyntaxRewriter
+        {
+            public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+            {
+                node = (ClassDeclarationSyntax)base.VisitClassDeclaration(node);
+                var visibilityKind = GetTypeDefaultVisibility(node);
+                return EnsureVisibility(node, node.Modifiers, (x, l) => x.WithModifiers(l), visibilityKind);
+            }
+
+            public override SyntaxNode VisitInterfaceDeclaration(InterfaceDeclarationSyntax node)
+            {
+                var visibilityKind = GetTypeDefaultVisibility(node);
+                return EnsureVisibility(node, node.Modifiers, (x, l) => x.WithModifiers(l), visibilityKind);
+            }
+
+            public override SyntaxNode VisitStructDeclaration(StructDeclarationSyntax node)
+            {
+                node = (StructDeclarationSyntax)base.VisitStructDeclaration(node);
+                var visibilityKind = GetTypeDefaultVisibility(node);
+                return EnsureVisibility(node, node.Modifiers, (x, l) => x.WithModifiers(l), visibilityKind);
+            }
+
+            public override SyntaxNode VisitDelegateDeclaration(DelegateDeclarationSyntax node)
+            {
+                var visibilityKind = GetTypeDefaultVisibility(node);
+                return EnsureVisibility(node, node.Modifiers, (x, l) => x.WithModifiers(l), visibilityKind);
+            }
+
+            public override SyntaxNode VisitEnumDeclaration(EnumDeclarationSyntax node)
+            {
+                var visibilityKind = GetTypeDefaultVisibility(node);
+                return EnsureVisibility(node, node.Modifiers, (x, l) => x.WithModifiers(l), visibilityKind);
+            }
+
+            public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+            {
+                node = (MethodDeclarationSyntax)base.VisitMethodDeclaration(node);
+                return EnsureVisibility(node, node.Modifiers, (x, l) => x.WithModifiers(l), SyntaxKind.PrivateKeyword);
+            }
+
+            private static SyntaxKind GetTypeDefaultVisibility(SyntaxNode node)
+            {
+                return IsNestedDeclaration(node) ? SyntaxKind.PrivateKeyword : SyntaxKind.InternalKeyword;
+            }
+
+            private static bool IsNestedDeclaration(SyntaxNode node)
+            {
+                var current = node.Parent;
+                while (current != null)
+                {
+                    if (SyntaxFacts.IsTypeDeclaration(current.CSharpKind()))
+                    {
+                        return true;
+                    }
+
+                    current = current.Parent;
+                }
+
+                return false;
+            }
+
+            private static MemberDeclarationSyntax EnsureVisibility<T>(T node, SyntaxTokenList originalModifiers, Func<T, SyntaxTokenList, T> withModifiers, SyntaxKind visibilityKind) where T : MemberDeclarationSyntax
+            {
+                Debug.Assert(SyntaxFacts.IsAccessibilityModifier(visibilityKind));
+
+                if (originalModifiers.Any(x => SyntaxFacts.IsAccessibilityModifier(x.CSharpKind())))
+                {
+                    return node;
+                }
+
+                SyntaxTokenList modifierList;
+                if (originalModifiers.Count == 0)
+                {
+                    var leadingTrivia = node.GetLeadingTrivia();
+                    node = node.WithLeadingTrivia();
+
+                    var visibilityToken = SyntaxFactory.Token(
+                        leadingTrivia,
+                        visibilityKind,
+                        SyntaxFactory.TriviaList(SyntaxFactory.SyntaxTrivia(SyntaxKind.WhitespaceTrivia, " ")));
+
+                    modifierList = SyntaxFactory.TokenList(visibilityToken);
+                }
+                else
+                {
+                    var leadingTrivia = originalModifiers[0].LeadingTrivia;
+                    var visibilityToken = SyntaxFactory.Token(
+                        leadingTrivia,
+                        visibilityKind,
+                        SyntaxFactory.TriviaList(SyntaxFactory.SyntaxTrivia(SyntaxKind.WhitespaceTrivia, " ")));
+
+                    var list = new List<SyntaxToken>();
+                    list.Add(visibilityToken);
+                    list.Add(originalModifiers[0].WithLeadingTrivia());
+                    for (int i = 1; i < originalModifiers.Count; i++)
+                    {
+                        list.Add(originalModifiers[i]);
+                    }
+
+                    modifierList = SyntaxFactory.TokenList(list);
+                }
+
+                return withModifiers(node, modifierList);
+            }
+        }
+
+        public async Task<Document> ProcessAsync(Document document, CancellationToken cancellationToken)
+        {
+            var syntaxNode = await document.GetSyntaxRootAsync(cancellationToken) as CSharpSyntaxNode;
+            if (syntaxNode == null)
+            {
+                return document;
+            }
+
+            var rewriter = new VisibilityRewriter();
+            var newNode = rewriter.Visit(syntaxNode);
+            return document.WithSyntaxRoot(newNode);
+        }
+    }
+}
