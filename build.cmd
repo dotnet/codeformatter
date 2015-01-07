f439237e901e6847855ea777e0a070b9ce36dba0
Matt Ellis <matell@microsoft.com>
2015-01-07 15:18:25 -0800

    Rename Solution File.
    Since this is the only tool we'll have here, it makes sense for us to
    name the solution file after the tool.
    
    Fixes #7

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@
 SETLOCAL
 
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
-SET SOLUTION_PATH=%~dp0src\BuildToolsInternal.sln
+SET SOLUTION_PATH=%~dp0src\CodeFormatter.sln
 
 IF EXIST %CACHED_NUGET% goto restore
 echo Downloading latest version of NuGet.exe...
