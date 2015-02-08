0b41a1dd77a5a1c3857f231a54290039fc4cb0ec
Immo Landwerth <immol@microsoft.com>
2015-02-07 21:27:08 -0800

    Rename variable to not include the VS version
    This minimizes the number of changes when the tool has to target a
    higher version of Visual Studio. Besides, it doesn't actually point
    to Visual Studio per-se.

--- build.cmd
+++ build.cmd
@@ -4,9 +4,9 @@ SETLOCAL
 
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 SET SOLUTION_PATH=%~dp0src\CodeFormatter.sln
-SET VS2015_BUILD_TOOLS_PATH="%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe"
+SET BUILD_TOOLS_PATH="%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe"
 
-IF NOT EXIST %VS2015_BUILD_TOOLS_PATH% (
+IF NOT EXIST %BUILD_TOOLS_PATH% (
   echo In order to build or run this tool you need either Visual Studio 2015 Preview or
   echo Microsoft Build Tools 2015 Preview tools installed.
   echo.
