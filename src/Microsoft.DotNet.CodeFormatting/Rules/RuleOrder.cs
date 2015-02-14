df1b298562b268633b3618e52a31eee7e06818ea
Jared Parsons <jaredpar@microsoft.com>
2015-02-14 12:41:08 -0800

    Removed old xunit logic

--- src/Microsoft.DotNet.CodeFormatting/Rules/RuleOrder.cs
+++ src/Microsoft.DotNet.CodeFormatting/Rules/RuleOrder.cs
@@ -34,7 +34,6 @@ namespace Microsoft.DotNet.CodeFormatting.Rules
     internal static class GlobalSemanticRuleOrder
     {
         public const int PrivateFieldNamingRule = 1;
-        public const int UsesXunitForTestsFormattingRule = 2;
 
     }
 }
