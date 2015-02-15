0b0ca8a9be3d2ec0e84df29217f09fccb87bfa09
Immo Landwerth <immol@microsoft.com>
2015-02-15 12:53:46 -0800

    Add rules for formatting attributes

---
+++ src/Microsoft.DotNet.CodeFormatting.Tests/Rules/AttributeSeparateListsRuleTests.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+using Microsoft.DotNet.CodeFormatting.Rules;
+
+using Xunit;
+
+namespace Microsoft.DotNet.CodeFormatting.Tests
+{
+    public class AttributeSeparateListsRuleTests : SyntaxRuleTestBase
+    {
+        internal override ISyntaxFormattingRule Rule
+        {
+            get { return new AttributeSeparateListsRule(); }
+        }
+
+        [Fact]
+        public void ParameterAttributeListsAreNotSeparated()
+        {
+            var text = @"
+namespace Namespace1
+{
+    class Class1
+    {
+        public int SomeMethod([In, Out]SomeType someParameter)
+        {
+            return 42;
+        }
+    }
+}";
+            Verify(text, text);
+        }
+
+        [Fact]
+        public void AttributeListsAreSeparated()
+        {
+            var text = @"
+[assembly: FileVersion(1, 1), AssemblyVersion(1, 1)]
+namespace Namespace1
+{
+    [
+        Serializable,       // Good, isn't?
+        DefaultValue(1)     // Is this the right value?
+    ]
+    class Class1
+    {
+        [Serializable, DefaultValue(1)]
+        public int SomeMethod(SomeType someParameter)
+        {
+            return 42;
+        }
+    }
+}";
+
+            var expected = @"
+[assembly: FileVersion(1, 1)]
+[assembly: AssemblyVersion(1, 1)]
+namespace Namespace1
+{
+
+    [Serializable]       // Good, isn't?
+    [DefaultValue(1)]     // Is this the right value?
+
+    class Class1
+    {
+        [Serializable]
+        [DefaultValue(1)]
+        public int SomeMethod(SomeType someParameter)
+        {
+            return 42;
+        }
+    }
+}";
+            Verify(text, expected);
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.CodeFormatting/Rules/AttributeNoParenthesesRule.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Microsoft.DotNet.CodeFormatting.Rules
+{
+    [SyntaxRuleOrder(SyntaxRuleOrder.AttributeNoParenthesesRule)]
+    internal sealed class AttributeNoParenthesesRule : ISyntaxFormattingRule
+    {
+        public SyntaxNode Process(SyntaxNode syntaxRoot)
+        {
+            var attributes = syntaxRoot.DescendantNodes()
+                                       .OfType<AttributeSyntax>()
+                                       .Where(a => a.ArgumentList != null &&
+                                                   a.ArgumentList.Arguments.Count == 0 &&
+                                                   (!a.ArgumentList.OpenParenToken.IsMissing || !a.ArgumentList.CloseParenToken.IsMissing));
+
+            return syntaxRoot.ReplaceNodes(attributes, (a, n) => a.WithArgumentList(null));
+        }
+    }
+}
