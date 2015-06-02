0f6a404e199f0547a171f33cebeb215a3acd5e33
Daniel Plaisted <daplaist@microsoft.com>
2015-06-02 10:49:53 -0700

    Add newlines to leading trivia of EndOfFileToken, if necessary

--- src/Microsoft.DotNet.CodeFormatting/Rules/NewLineAtEndOfFileRule.cs
+++ src/Microsoft.DotNet.CodeFormatting/Rules/NewLineAtEndOfFileRule.cs
@@ -14,6 +14,17 @@ namespace Microsoft.DotNet.CodeFormatting.Rules
         public SyntaxNode Process(SyntaxNode syntaxRoot, string languageName)
         {
             bool needsNewLine;
+            var endOfFileToken = syntaxRoot.GetLastToken(true, true, true, true);
+            if (!endOfFileToken.IsKind(SyntaxKind.EndOfFileToken))
+            {
+                throw new InvalidOperationException("Expected last token to be EndOfFileToken, was actually: " + endOfFileToken.Kind());
+            }
+
+            if (endOfFileToken.HasLeadingTrivia)
+            {
+                return AddNewLineToEndOfFileTokenLeadingTriviaIfNecessary(syntaxRoot, endOfFileToken);
+            }
+
             var lastToken = syntaxRoot.GetLastToken();
             if (!lastToken.HasTrailingTrivia)
             {
@@ -38,6 +49,21 @@ namespace Microsoft.DotNet.CodeFormatting.Rules
                 var newLastToken = lastToken.WithTrailingTrivia(lastToken.TrailingTrivia.Concat(new[] { newLine }));
                 return syntaxRoot.ReplaceToken(lastToken, newLastToken);
             }
+            return syntaxRoot;
+        }
+
+        SyntaxNode AddNewLineToEndOfFileTokenLeadingTriviaIfNecessary(SyntaxNode syntaxRoot, SyntaxToken endofFileToken)
+        {
+            if (endofFileToken.LeadingTrivia.Last().IsKind(SyntaxKind.EndOfLineTrivia))
+            {
+                return syntaxRoot;
+            }
+
+            var newLine = SyntaxUtil.GetBestNewLineTriviaRecursive(endofFileToken.Parent);
+            var newLastToken = endofFileToken.WithTrailingTrivia(endofFileToken.TrailingTrivia.Concat(new[] { newLine }));
+            return syntaxRoot.ReplaceToken(endofFileToken, newLastToken);
+
+
             return syntaxRoot;
         }
     }
