59ed9d2afbee2e8ebf87b05242b693cfc386c461
Daniel Plaisted <daplaist@microsoft.com>
2015-06-01 19:31:09 -0700

    Add tests for NewLineAtEndOfFileRule

---
+++ src/Microsoft.DotNet.CodeFormatting.Tests/Rules/NewLineAtEndOfFileRuleTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.DotNet.CodeFormatting.Tests
+{
+    public class NewLineAtEndOfFileRuleTests : SyntaxRuleTestBase
+    {
+        internal override ISyntaxFormattingRule Rule
+        {
+            get { return new Rules.NewLineAtEndOfFileRule(); }
+        }
+
+        [Fact]
+        public void SimpleClassWithoutNewLineAtEndOfFile()
+        {
+            var text = @"using System;
+public class TestClass
+{
+}";
+
+            var expected = @"using System;
+public class TestClass
+{
+}
+";
+
+            Verify(text, expected);
+        }
+
+        [Fact]
+        public void SimpleClassWithNewLineAtEndOfFile()
+        {
+            var text = @"using System;
+public class TestClass
+{
+}
+";
+
+            Verify(text, text);
+        }
+
+        [Fact]
+        public void CommentAtEndOfFile()
+        {
+            var text = @"using System;
+public class TestClass
+{
+}
+//  Hello World";
+
+            var expected = @"using System;
+public class TestClass
+{
+}
+//  Hello World
+";
+
+            Verify(text, expected);
+        }
+
+        [Fact]
+        public void IfDefAtEndOfFile()
+        {
+            var text = @"using System;
+public class TestClass
+{
+}
+#if TEST
+#endif";
+
+            var expected = @"using System;
+public class TestClass
+{
+}
+#if TEST
+#endif
+";
+
+            Verify(text, expected);
+        }
+    }
+}
