509440053669a8f1bfe14b602409120ffbbf9181
Jake <jake@helfert.us>
2015-11-07 08:48:59 -0700

    Added explicit visibility to RegionFinder.

--- src/Microsoft.DotNet.CodeFormatting/Rules/RemoveRegionsRule.cs
+++ src/Microsoft.DotNet.CodeFormatting/Rules/RemoveRegionsRule.cs
@@ -9,44 +9,44 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Microsoft.DotNet.CodeFormatting.Rules
 {
-	[SyntaxRule(RemoveRegionsRule.Name, RemoveRegionsRule.Description, SyntaxRuleOrder.RemoveRegionsRule)]
-	internal sealed class RemoveRegionsRule : CSharpOnlyFormattingRule, ISyntaxFormattingRule
-	{
-		internal const string Name = "RemoveRegions";
-		internal const string Description = "Removes all regions";
+    [SyntaxRule(RemoveRegionsRule.Name, RemoveRegionsRule.Description, SyntaxRuleOrder.RemoveRegionsRule)]
+    internal sealed class RemoveRegionsRule : CSharpOnlyFormattingRule, ISyntaxFormattingRule
+    {
+        internal const string Name = "RemoveRegions";
+        internal const string Description = "Removes all regions";
 
-		public SyntaxNode Process(SyntaxNode targetNode, string languageName)
-		{
-			var finder = new RegionFinder();
-			finder.Visit(targetNode);
-			var results = finder.Results;
+        public SyntaxNode Process(SyntaxNode targetNode, string languageName)
+        {
+            var finder = new RegionFinder();
+            finder.Visit(targetNode);
+            var results = finder.Results;
 
-			if (results.Count > 0)
-			{
-				return targetNode.ReplaceTrivia(results,
-					(arg1, arg2) => new SyntaxTrivia());
-			}
-			return targetNode;
-		}
+            if (results.Count > 0)
+            {
+                return targetNode.ReplaceTrivia(results,
+                    (arg1, arg2) => new SyntaxTrivia());
+            }
+            return targetNode;
+        }
 
-		class RegionFinder : CSharpSyntaxWalker
-		{
-			public List<SyntaxTrivia> Results { get; } = new List<SyntaxTrivia>();
+        private class RegionFinder : CSharpSyntaxWalker
+        {
+            public List<SyntaxTrivia> Results { get; } = new List<SyntaxTrivia>();
 
-			public RegionFinder()
-				: base(SyntaxWalkerDepth.StructuredTrivia)
-			{
-			}
+            public RegionFinder()
+                : base(SyntaxWalkerDepth.StructuredTrivia)
+            {
+            }
 
-			public override void VisitRegionDirectiveTrivia(RegionDirectiveTriviaSyntax node)
-			{
-				Results.Add(node.ParentTrivia);
-			}
+            public override void VisitRegionDirectiveTrivia(RegionDirectiveTriviaSyntax node)
+            {
+                Results.Add(node.ParentTrivia);
+            }
 
-			public override void VisitEndRegionDirectiveTrivia(EndRegionDirectiveTriviaSyntax node)
-			{
-				Results.Add(node.ParentTrivia);
-			}
-		}
-	}
+            public override void VisitEndRegionDirectiveTrivia(EndRegionDirectiveTriviaSyntax node)
+            {
+                Results.Add(node.ParentTrivia);
+            }
+        }
+    }
 }
