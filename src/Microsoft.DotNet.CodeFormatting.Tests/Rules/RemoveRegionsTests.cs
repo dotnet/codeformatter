6cd9c4988a3c497e9719e51dee25384b147e94f0
Jake <jake@helfert.us>
2015-11-06 17:15:31 -0700

    Added a rule to remove #region/#endregion directives

---
+++ src/Microsoft.DotNet.CodeFormatting.Tests/Rules/RemoveRegionsTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+
+namespace Microsoft.DotNet.CodeFormatting.Tests
+{
+	public class RemoveRegionsTests : SyntaxRuleTestBase
+	{
+		internal override ISyntaxFormattingRule Rule
+		{
+			get
+			{
+				return new Rules.RemoveRegionsRule();
+			}
+		}
+
+		[Fact]
+		public void TestRemoveRegions()
+		{
+			var text = @"
+#region Region 1
+
+//comment
+#endregion Region 1
+
+class WithRegions
+{
+	#region have region here
+	public static void DoNothing()
+	{
+		#region inside method
+
+		#endregion inside method
+	}
+#endregion   
+}
+";
+			var expected = @"
+
+//comment
+
+class WithRegions
+{
+    public static void DoNothing()
+    {
+
+    }
+}
+";
+			Verify(text, expected);
+		}
+	}
+}
---
+++ src/Microsoft.DotNet.CodeFormatting/Rules/RemoveRegionsRule.cs
@@ -0,0 +1,52 @@
+﻿// Copyright(c) Microsoft.All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Microsoft.DotNet.CodeFormatting.Rules
+{
+	[SyntaxRule(RemoveRegionsRule.Name, RemoveRegionsRule.Description, SyntaxRuleOrder.RemoveRegionsRule)]
+	internal sealed class RemoveRegionsRule : CSharpOnlyFormattingRule, ISyntaxFormattingRule
+	{
+		internal const string Name = "RemoveRegions";
+		internal const string Description = "Removes all regions";
+
+		public SyntaxNode Process(SyntaxNode targetNode, string languageName)
+		{
+			var finder = new RegionFinder();
+			finder.Visit(targetNode);
+			var results = finder.Results;
+
+			if (results.Count > 0)
+			{
+				return targetNode.ReplaceTrivia(results,
+					(arg1, arg2) => new SyntaxTrivia());
+			}
+			return targetNode;
+		}
+
+		class RegionFinder : CSharpSyntaxWalker
+		{
+			public List<SyntaxTrivia> Results { get; } = new List<SyntaxTrivia>();
+
+			public RegionFinder()
+				: base(SyntaxWalkerDepth.StructuredTrivia)
+			{
+			}
+
+			public override void VisitRegionDirectiveTrivia(RegionDirectiveTriviaSyntax node)
+			{
+				Results.Add(node.ParentTrivia);
+			}
+
+			public override void VisitEndRegionDirectiveTrivia(EndRegionDirectiveTriviaSyntax node)
+			{
+				Results.Add(node.ParentTrivia);
+			}
+		}
+	}
+}
