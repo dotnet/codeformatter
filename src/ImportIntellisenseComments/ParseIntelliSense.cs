aefe35c98083799ea7832ea5172e463831a95996
Phillip Carter <phillip.carter@outlook.com>
2016-02-24 15:55:11 -0800

    Add intellisense-to-source import tool
    This tool reads existing intellisense files (XML doc files generated by a
    build), extracts the XML doc comments, and writes them to source files
    (specified by a directory).

---
+++ src/ImportIntellisenseComments/ParseIntelliSense.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Xml;
+
+namespace ImportIntellisenseComments
+{
+    class ParseIntelliSense
+    {
+
+        private readonly string _intelliSenseDirectory;
+        private Dictionary<string, string> _membersDictionary = new Dictionary<string, string>();
+
+        public string IntelliSenseDirectory
+        {
+            get
+            {
+                return _intelliSenseDirectory;
+            }
+        }
+
+        public Dictionary<string, string> MembersDictionary
+        {
+            get
+            {
+                return _membersDictionary;
+            }
+        }
+
+        public ParseIntelliSense(string intelliSenseDirectory)
+        {
+            _intelliSenseDirectory = intelliSenseDirectory;
+        }
+
+        #region Methods
+        public void ParseIntelliSenseFiles()
+        {
+            foreach (var uidAndReader in from f in EnumerateDevelopCommentFiles()
+                                         from uidAndReader in EnumerateDeveloperComments(f)
+                                         select uidAndReader)
+            {
+                try
+                {
+                    _membersDictionary.Add(uidAndReader.Uid, uidAndReader.Reader.ReadOuterXml());
+                }
+                catch (ArgumentException)
+                {
+                    // There should only be a few duplicate that it's fine to just ignore.
+                    System.Diagnostics.Debug.WriteLine($"Duplicated entry: {uidAndReader.Uid}");
+                }
+            }
+        }
+
+        private IEnumerable<string> EnumerateDevelopCommentFiles() =>
+            Directory.EnumerateFiles(IntelliSenseDirectory, "*.xml", SearchOption.TopDirectoryOnly);
+
+        private IEnumerable<UidAndReader> EnumerateDeveloperComments(string file)
+        {
+            //Console.WriteLine($"Loading developer comments from file: {file}");
+            return from reader in
+                       new Func<XmlReader>(() => XmlReader.Create(file))
+                       .EmptyIfThrow()
+                       .ProtectResource()
+                   where reader.ReadToFollowing("members")
+                   from apiReader in reader.Elements("member")
+                   let commentId = apiReader.GetAttribute("name")
+                   where commentId != null && commentId.Length > 2 && commentId[1] == ':'
+                   select new UidAndReader { Uid = commentId, Reader = apiReader };
+        }
+        #endregion
+
+        #region Nested Class
+
+        internal sealed class UidAndReader
+        {
+            public string Uid { get; set; }
+            public XmlReader Reader { get; set; }
+        }
+        #endregion
+    }
+}
---
+++ src/ImportIntellisenseComments/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Options;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.Text;
+
+namespace ImportIntellisenseComments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("{0} <IntelliSenseDirectory> <SourceDirectory>", AppDomain.CurrentDomain.FriendlyName);
+                Console.ReadLine();
+                return;
+            }
+
+            ParseIntelliSense p = new ParseIntelliSense(args[0]);
+
+            //Check parameters
+            if (!Directory.Exists(p.IntelliSenseDirectory))
+            {
+                Console.WriteLine($"Directory not found: {p.IntelliSenseDirectory}");
+                Console.ReadLine();
+                return;
+            }            
+            if (!Directory.Exists(args[1]))
+            {
+                Console.WriteLine($"Directory not found: {args[1]}");
+                Console.ReadLine();
+                return;
+            }
+            p.ParseIntelliSenseFiles();
+
+            // Adds all the references needed for CommentID to build correctly
+            var metadataReferences = new MetadataReference[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) };
+
+            foreach (var file in EnumerateSourceFiles(args[1]))
+            {
+
+                // Reads the source code from the file
+                SourceText text;
+                using (var stream = File.OpenRead(file))
+                {
+                    text = SourceText.From(stream);
+                }
+
+                SyntaxTree tree = (SyntaxTree)CSharpSyntaxTree.ParseText(text);
+
+                var compilation = CSharpCompilation.Create("test", syntaxTrees: new[] { tree }, 
+                    references: metadataReferences);
+                var rewriter = new Rewriter(compilation.GetSemanticModel(tree), p.MembersDictionary);
+                var newTreeRootNode = rewriter.Visit(tree.GetRoot());
+                var newTree = newTreeRootNode.SyntaxTree;
+
+                //Checks to see if the source code was changed
+                if (tree != newTree)
+                {
+                    Workspace workspace = MSBuildWorkspace.Create();
+                    OptionSet options = workspace.Options;
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInAccessors, false);
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInAnonymousMethods, false);
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInAnonymousTypes, false);
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInControlBlocks, false);
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInLambdaExpressionBody, false);
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInMethods, true);
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInObjectCollectionArrayInitializers, false);
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInProperties, true);
+                    options = options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInTypes, true);
+
+                    SyntaxNode formattedNode = Formatter.Format(newTree.GetRoot(), workspace, options);
+
+                    Console.WriteLine($"Saving file: {file}");
+                    SourceText newText = formattedNode.GetText();
+                    using (var writer = new StreamWriter(file, append: false, encoding: text.Encoding))
+                    {
+                        newText.Write(writer);
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Press ENTER to exit;");
+            Console.ReadLine();
+        }
+
+        private static IEnumerable<string> EnumerateSourceFiles(string path) =>
+            Directory.EnumerateFiles(path, "*.cs", SearchOption.AllDirectories);
+
+    }
+
+}
