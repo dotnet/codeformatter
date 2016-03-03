45a8f4cdd81c584786f812e443eebc0ae06cc663
Dan Quirk <danquirk@microsoft.com>
2016-03-03 10:17:12 -0800

    Update to Roslyn 1.2-beta1 so analyzers built against any older Roslyn version should work

---
+++ src/Microsoft.DotNet.CodeFormatting/AnalyzerFileLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.DotNet.CodeFormatting
+{
+    public class AnalyzerFileLoader : IAnalyzerAssemblyLoader
+    {
+        public void AddDependencyLocation(string fullPath)
+        {
+        }
+
+        public Assembly LoadFromPath(string fullPath)
+        {
+            return Assembly.LoadFrom(fullPath);
+        }
+    }
+}
